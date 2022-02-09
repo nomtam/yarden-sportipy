e57cb715e705f949564c26c92efc55fe431549de
yardenc2003 <yardenc2003@gmail.com>
2022-02-09 14:36:11 +0200

    Added config file

---
+++ helpers/config.py
@@ -0,0 +1,6 @@
+class CollectionPaths:
+    SONG_PATH_TEMPLATE = "../resources/songs/song_%s.json"
+    ALBUM_PATH_TEMPLATE = "../resources/albums/album_%s.json"
+    USER_PATH_TEMPLATE = "../resources/users/user_%s.json"
+    ARTIST_PATH_TEMPLATE = "../resources/artists/artist_%s.json"
+
