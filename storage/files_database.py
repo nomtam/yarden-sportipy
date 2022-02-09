16d47f995aac7a481f1a12c1bb9b02c96b03c2a6
yardenc2003 <yardenc2003@gmail.com>
2022-02-09 18:28:53 +0200

    Developed CollectionDataBase class (to support entity updates)

--- storage/files_database.py
+++ storage/files_database.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 from storage.reading import Reading
 from storage.writing import Writing
 
@@ -15,9 +17,17 @@ class CollectionDataBase:
         self.writer = writer
         self.entities = {}
 
+    def dump_entity(self, entity):
+        full_path = self.meta_data.path_template % entity.id
+        self.writer(full_path, entity)
+
     def load_entity(self, entity_id: str):
         full_path = self.meta_data.path_template % entity_id
         self.entities[entity_id] = self.reader(full_path)
+
+    def set_entity(self, entity):
+        self.entities[entity.id] = entity  # updates the entities dict
+        self.dump_entity(entity)  # updates the entity file
 
     # TODO: check if valid id (raise exception if not)
     def get_entity(self, entity_id: str):
@@ -29,8 +39,12 @@ class CollectionDataBase:
 # this db is capable of loading collections of objects from various file types.
 class AppDataBase:
     def __init__(self, **collections):
-        self.collections = collections
+        self.collections: Dict[str, CollectionDataBase] = collections
 
+    def update_entity(self, collection_key: str, entity):
+        self.collections[collection_key].set_entity(entity)
+
+    # TODO: delete
     def print_collections(self):
         print("collections: ", list(self.collections.keys()))
 
