b84b1bcd23585afc93c8623b96076d1010c4320d
yardenc2003 <yardenc2003@gmail.com>
2022-02-09 09:58:25 +0200

    Added Song class

---
+++ models/music/music_models.py
@@ -0,0 +1,8 @@
+class Song:
+    def __init__(self, unique_id, name, album, artists, popularity):
+        self.id = unique_id
+        self.name = name
+        self.album = album
+        self.artists = artists
+        self.popularity = popularity
+
