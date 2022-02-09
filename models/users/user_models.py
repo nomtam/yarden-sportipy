cd3a5a7d9063788fb96ec4cdae41dcfc5f487e3d
yardenc2003 <yardenc2003@gmail.com>
2022-02-09 10:53:51 +0200

    Added FreeUser class

--- models/users/user_models.py
+++ models/users/user_models.py
@@ -4,7 +4,13 @@ from models.music.music_models import Playlist
 
 
 class User(ABC):
-    def __init__(self, unique_id: str, username: str, playlists: Dict[str, Playlist]):
+    def __init__(self, unique_id: str, username: str, playlists: Dict[str, Playlist], user_type="free"):
         self.id = unique_id
         self.username = username
+        self.type = user_type
         self.playlists = playlists
+
+
+class FreeUser(User):
+    def __init__(self, unique_id: str, username: str, playlists: Dict[str, Playlist]):
+        super().__init__(unique_id=unique_id, username=username, playlists=playlists)
