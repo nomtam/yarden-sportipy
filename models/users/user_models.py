69ab8bc97b0e9efc2a1c71c2656e9475d6640a76
yardenc2003 <yardenc2003@gmail.com>
2022-02-09 10:57:08 +0200

    fixed type hinting

--- models/users/user_models.py
+++ models/users/user_models.py
@@ -4,24 +4,24 @@ from models.music.music_models import Playlist
 
 
 class User(ABC):
-    def __init__(self, unique_id: str, username: str, playlists: Dict[str, Playlist], user_type="free"):
-        self.id = unique_id
-        self.username = username
-        self.type = user_type
-        self.playlists = playlists
+    def __init__(self, unique_id, username, playlists, user_type="free"):
+        self.id: str = unique_id
+        self.username: str = username
+        self.type: str = user_type
+        self.playlists: Dict[str, Playlist] = playlists
 
 
 class FreeUser(User):
-    def __init__(self, unique_id: str, username: str, playlists: Dict[str, Playlist]):
+    def __init__(self, unique_id, username, playlists):
         super().__init__(unique_id=unique_id, username=username, playlists=playlists)
 
 
 class PremiumUser(User):
-    def __init__(self, unique_id: str, username: str, playlists: Dict[str, Playlist]):
+    def __init__(self, unique_id, username, playlists):
         super().__init__(unique_id=unique_id, username=username, playlists=playlists, user_type="premium")
 
 
 class Artist(PremiumUser):
-    def __init__(self, unique_id: str, username: str, playlists: Dict[str, Playlist], albums):
+    def __init__(self, unique_id, username, playlists, albums):
         super().__init__(unique_id=unique_id, username=username, playlists=playlists)
         self.albums: Dict[str, str] = albums
